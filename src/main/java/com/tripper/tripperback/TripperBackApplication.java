3ab7de2192712edc9946f320ece669792e06123c
dytjq0123 <dytjq0123@gmail.com>
2022-04-06 16:22:09 +0900

    first commit

---
+++ src/main/java/com/tripper/tripperback/TripperBackApplication.java
@@ -0,0 +1,13 @@
+package com.tripper.tripperback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripperBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripperBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripper/tripperback/TripperBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripper.tripperback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripperBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
